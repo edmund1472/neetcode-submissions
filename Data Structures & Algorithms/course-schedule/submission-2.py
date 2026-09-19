144d26d2592871e24926155b94379bbc4c045871
edmund1472 <130937412+edmund1472@users.noreply.github.com>
2026-09-19 15:35:09 -0400

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        graph = {i: [] for i in range(numCourses)}
+
+        for course, prereq in prerequisites:
+            graph[course].append(prereq)
+
+        visiting = set()
+
+        def dfs(course):
+
+            if course in visiting:
+                return False
+
+            visiting.add(course)
+
+            for prereq in graph[course]:
+                result = dfs(prereq)
+                if result == False:
+                    return False
+                else:
+                    continue
+            visiting.remove(course)
+
+            graph[course] = []
+
+            return True
+
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+
+        return True
